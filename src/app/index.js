31a7464d917cb5a54b57088d896faef9b4e11ec9
nikhilbang44 <nikhilbang44@gmail.com>
2018-03-14 14:22:20 +0530

    First Component

--- src/app/index.js
+++ src/app/index.js
@@ -1,1 +1,17 @@
-console.log("IT worrks");
+import React from 'react';
+import { render } from 'react-dom';
+
+class App extends React.Component {
+    render() {
+        return ( 	<div className="container">
+        				<div className="row">
+        					<div className="col-xs-10 col-xs-offect-1">
+         						<h1> Hello World!!! </h1>
+         					</div>
+         				</div>
+         			</div>
+         	   );
+    }
+}
+
+render( < App / > , window.document.getElementById("app"));
