2a47c10617bc2282722060ffa1079558a4fb6dc9
nikhilbang44 <nikhilbang44@gmail.com>
2018-03-21 20:58:13 +0530

    ReactJS Basics - #7 Events & ReactJS

--- src/app/components/Home.js
+++ src/app/components/Home.js
@@ -2,29 +2,33 @@ import React from "react";
 import PropTypes from 'prop-types';
 
 export class Home extends React.Component {
+	constructor(props)
+	{
+		super();
+		this.age = props.age;
+	}
+
+	onMakeOlder()
+	{
+		this.age += 3;
+		console.log(this)
+	}
+
 	render() {
-		var text = "something!";
-		console.log(this.props);
 		return (
 			<div>
 				<p> In a new Component!</p>
-				<p> {text}</p>
 				<p> Your name is {this.props.name}, your age is {this.props.age}</p>
-				<p>User Object => Name: {this.props.user.name}</p>
-				<div>
-					<h4>Hobbies</h4>
-					<ul>
-						{this.props.user.hobbies.map((hobby, i) => <li key={i}> {hobby}</li>)}
-					</ul>
-				</div>
+				<hr/>
+	          <button onClick ={() => this.onMakeOlder()} className="btn btn-primary"> Make me older!</button>
 			</div>
 	  );
 	}
+	 // <button onClick = {this.onMakeOlder.bind(this)} className="btn btn-primary"> Make me older!</button>
+
 }
 
 Home.PropTypes = {
 	name: PropTypes.string,
-	age:  PropTypes.number,
-	user: PropTypes.object,
-	children: PropTypes.element.isRequired
+	age:  PropTypes.number
 };
