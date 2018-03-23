68930c7ccaf479a7e2c78a414bc95851c66ae184
Nikhil44 <nbang0506@gmail.com>
2018-03-23 14:09:16 +0530

    #14 Component Lifecycle

--- src/app/index.js
+++ src/app/index.js
@@ -10,7 +10,8 @@ class App extends React.Component {
 
         super();
         this.state = {
-            homeLink: "Home"
+            homeLink: "Home",
+            homeMounted: true
         };
     }
 
@@ -23,7 +24,28 @@ class App extends React.Component {
             homeLink: newName
         });
     }
+
+    onChangeHomeMounted(){
+        this.setState({
+            homeMounted: !this.state.homeMounted
+            });
+    }
+
     render() {
+
+        let homeCmp = "";
+        if (this.state.homeMounted)
+        {
+            homeCmp = (
+                 <Home name={"Max"}
+                initialAge={27}
+                greet={this.onGreet}
+                changeLink={this.onChangeLinkName.bind(this)}
+                initialLinkName={this.state.homeLink}
+            />
+            );
+        }
+        
         return ( 	<div className="container">
         				<div className="row">
         					<div className="col-xs-10 col-xs-offect-1">
@@ -32,12 +54,14 @@ class App extends React.Component {
          				</div>
                         <div className="row">
                             <div className="col-xs-10 col-xs-offect-1">
-                                <Home name={"Max"}
-                                 initialAge={27}
-                                  greet = {this.onGreet}
-                                  changeLink={this.onChangeLinkName.bind(this)}
-                                  initialLinkName={this.state.homeLink}
-                                  />
+                               {homeCmp}
+                            </div>
+                        </div>
+                        <div className="row">
+                            <div className="col-xs-10 col-xs-offect-1">
+                                <button onClick={this.onChangeHomeMounted.bind(this)}
+                                        className="btn btn-primary">(UN)Mount Home Component 
+                                </button>
                             </div>
                         </div>
                  </div>
